29bbe5ce07347f06a93b94e406078364e9ee0679
Dejan Senborn <dejan.senborn@crealogix.com>
2024-01-07 17:40:17 +0100

    Chapter08 finished

---
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderRepository.java
@@ -0,0 +1,4 @@
+package com.polarbookshop.orderservice.order.domain;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+public interface OrderRepository
+        extends ReactiveCrudRepository<Order,Long> {}
